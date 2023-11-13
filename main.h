2f6d1714f6f1107117b51e528c65cbe64761c3ae
IMTREDU <imtr.edu@gmail.com>
2023-11-13 21:31:47 +0000

    Main's

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /* PRINTF_H */
