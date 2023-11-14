38ba43ead35a1c1e49a336b6e29e2a38545114c2
Wisamtaher <wisam.radwan@gmail.com>
2023-11-14 12:24:46 -0800

    flags

--- main.h
+++ main.h
@@ -43,4 +43,12 @@ int print_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+
 #endif
