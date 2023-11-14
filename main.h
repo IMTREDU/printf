db92f2d9b6ae41f797af72a7b9b5ba767412438b
Wisamtaher <wisam.radwan@gmail.com>
2023-11-14 13:20:04 -0800

    check

--- main.h
+++ main.h
@@ -7,6 +7,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
 /**
  * struct format - converter for printf
  * @ph: type char pointer of the specifier
@@ -45,17 +56,10 @@ int _printf(const char *format, ...);
 int get_flags(const char *format, int *i);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
+int is_printable(char c);
+int append_hexa_code(char ascii_code, char buffer[], int i);
 int is_digit(char);
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif
